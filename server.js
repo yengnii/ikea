8a36cea54ca09b213c046cfcd04f5056b5fae075
yengnii <yengni718@gmail.com>
2022-08-17 23:35:37 +0900

    server setting

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const cors = require('cors');
+app.use(cors());
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'views') + 'index.html' );
+})
+
+
+app.get('/*', (req, res)=> {
+    console.log(req.url);
+    res.status(404).sendFile(path.join(__dirname, 'views'), '404.html');
+})
+app.listen( PORT , () => {
+    console.log(`server runing on PORT ${PORT}`);
+})
