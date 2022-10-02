444a12f33c152134378eac6bc5d27e3645b57fbc
yengnii <yengni718@gmail.com>
2022-10-02 09:56:25 +0900

    fix language pack, add service tab in sub page

--- public/script/language.js
+++ public/script/language.js
@@ -1,8 +1,15 @@
-const select = document.querySelector('#select_country');
-select.addEventListener('click', function(event){
-  // event.preventDefault();
-   const current = event.target;
-   let currentValue = current.getAttribute('data-value');
-   console.log( currentValue );
-   location.href = currentValue;
-})
+// var language_toggle = false; 
+// $('.language_choice').on('click', function(){
+//   console.log('click');
+//   if(!language_toggle){
+//       $('.language_choice+ul').css( 'bottom', '-60px');
+//   }else{
+//       $('.language_choice+ul').css( 'bottom', '0px');
+//   }
+//   language_toggle = !language_toggle ;
+// })
+
+// $('.language_choice+ul>li').on('click', function( ){
+//   console.log( $(this).attr('data-value') );
+//   location.href = $(this).attr('data-value');
+// })
--- server.js
+++ server.js
@@ -6,28 +6,36 @@ const cors = require('cors');
 app.use(cors());
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'views')));
+
+
+app.use( express.json());
+app.use( express.urlencoded({extended:false}));
+// app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res)=> {
-    res.sendFile(path.join(__dirname, 'views') + 'index.html' );
+    res.sendFile(path.join(__dirname, 'views' + '/index.html' ));
 })
 
 app.get('/index', (req, res)=> {
-    res.sendFile(path.join(__dirname, 'views') + 'index.html' );
+    res.sendFile(path.join(__dirname, 'views' + '/index.html' ));
+})
+
+app.get('/select', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'views' + '/select.html' ));
 })
 
 app.get('/sub', (req, res)=>{ 
-    res.sendFile( path.join( __dirname, 'views', 'sub.html' ))
+    res.sendFile( path.join( __dirname, 'views', '/sub.html' ))
 })
 
 app.get('/indexEng', (req, res)=>{ 
-    res.sendFile( path.join( __dirname, 'views', 'indexEng.html' ))
+    res.sendFile( path.join( __dirname, 'views', '/indexEng.html' ))
 })
 
 app.get('/*', (req, res)=> {
     console.log(req.url);
-    res.status(404).sendFile(path.join(__dirname, 'views'), '404.html');
+    res.status(404).sendFile(path.join(__dirname, 'views', '/404.html'));
 })
 app.listen( PORT , () => {
     console.log(`server runing on PORT ${PORT}`);
